7c85250c31cae18b193f0bff9687d0270dea592f
dion-ricky <dionrickysptr@gmail.com>
2021-04-27 13:40:06 +0000

    🎮 Trying getUserMedia API

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <LiveCamera />
+</template>
+
+<script>
+import LiveCamera from './components/LiveCamera.vue'
+
+export default {
+  name: 'App',
+  components: {
+    LiveCamera
+  }
+}
+</script>
+
+<style>
+@import url('./assets/test.css');
+</style>
---
+++ src/components/LiveCamera.vue
@@ -0,0 +1,21 @@
+<template>
+    <video ref="player" controls autoplay></video>    
+</template>
+
+<script>
+    export default {
+        name: "LiveCamera",
+        created() {
+            const player = this.$refs.player;
+
+            const constraints = {
+                video: true
+            }
+
+            navigator.mediaDevices.getUserMedia(constraints)
+            .then((stream) => {
+                player.srcObject = stream;
+            })
+        }
+    }
+</script>
