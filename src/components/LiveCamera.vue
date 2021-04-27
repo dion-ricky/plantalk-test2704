4cfd219f4bf9b522e4e67c28cba48b7cf560206b
dion-ricky <dionrickysptr@gmail.com>
2021-04-27 14:29:51 +0000

    ⚙ Fix DOM null reference

--- src/components/LiveCamera.vue
+++ src/components/LiveCamera.vue
@@ -1,21 +1,34 @@
 <template>
-    <video ref="player" controls autoplay></video>    
+    <video ref="player" autoplay></video>
+    <button @click="openCamera">Open</button>
+    <button @click="closeCamera">Close</button>
 </template>
 
 <script>
     export default {
         name: "LiveCamera",
-        created() {
-            const player = this.$refs.player;
+        methods: {
+            openCamera() {
+                const player = this.$refs.player;
 
-            const constraints = {
-                video: true
+                console.log(player);
+                const constraints = {
+                    video: {
+                        facingMode: "environment"
+                    }
+                    // configure video dimensions here as well
+                    // https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia
+                }
+
+                navigator.mediaDevices.getUserMedia(constraints)
+                .then((stream) => {
+                    player.srcObject = stream;
+                })
+            },
+            closeCamera() {
+                this.$refs.player.srcObject.getVideoTracks()
+                .forEach(track => track.stop())
             }
-
-            navigator.mediaDevices.getUserMedia(constraints)
-            .then((stream) => {
-                player.srcObject = stream;
-            })
         }
     }
 </script>
