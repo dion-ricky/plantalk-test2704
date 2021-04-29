7edb87c5cc77e388f18949ea7b9289c66db77b22
dion-ricky <dionrickysptr@gmail.com>
2021-04-29 02:51:54 +0000

    ⚙ Added configuration for PWA

---
+++ vue.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+
+    pwa: {
+        name: "plantalk-test2704",
+        themeColor: "#4DBA87",
+        workboxPluginMode: 'GenerateSW',
+        manifestOptions: {
+            // edit this value to change manifest.json
+            // name: using default pwa.name,
+            // short_name: using default pwa.name,
+            start_url: "/",
+            display: "standalone",
+            // theme_color: using default pwa.themeColor
+        }
+    }
+}
