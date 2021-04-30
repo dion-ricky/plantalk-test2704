46d5bfe6fa387ae158f0aef16fdd69b31058c508
dion-ricky <dionrickysptr@gmail.com>
2021-04-30 13:03:28 +0000

    🔼 Refactor code to camera lib

--- src/views/LiveCamera.vue
+++ src/views/LiveCamera.vue
@@ -2,37 +2,73 @@
     <video ref="player" :srcObject="stream" autoplay></video>
     <button @click="openCamera">Open</button>
     <button @click="closeCamera">Close</button>
+    <button @click="toggleTorch">Torch</button>
+    <button @click="getCap">Get Capabilities</button>
+    <button @click="openCameraEnv">Open Environment</button>
+    <button @click="openCameraUser">Open User</button>
+    <p ref="dev"></p>
 </template>
 
 <script>
-    export default {
-        name: "LiveCamera",
-        data: () => ({
-            stream: null
-        }),
-        methods: {
-            openCamera() {
-                const player = this.$refs.player;
+import PlantalkCamera from '../camera'
 
-                console.log(player);
-                const constraints = {
-                    video: {
-                        facingMode: "environment"
-                    }
-                    // configure video dimensions here as well
-                    // https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia
+export default {
+    name: "LiveCamera",
+    data: () => ({
+        camera: PlantalkCamera,
+        stream: null,
+        isTorchOn: false
+    }),
+    methods: {
+        openCamera() {
+            this.camera.getUserMediaStream()
+            .then((mediaStream) => {
+                this.stream = mediaStream
+            });
+        },
+        closeCamera() {
+            this.$refs.player.srcObject.getVideoTracks()
+            .forEach(track => track.stop())
+            // this.camera.stopVideo();
+        },
+        toggleTorch() {
+            let toggle = !this.isTorchOn;
+            console.log(toggle)
+            this.camera.toggleTorch(toggle);
+            this.isTorchOn = toggle;
+        },
+        openCameraEnv() {
+            this.camera.getUserMediaStream({
+                video: {
+                    facingMode: "environment"
                 }
-
-                navigator.mediaDevices.getUserMedia(constraints)
-                .then((stream) => {
-                    // player.srcObject = stream;
-                    this.stream = stream;
-                })
-            },
-            closeCamera() {
-                this.$refs.player.srcObject.getVideoTracks()
-                .forEach(track => track.stop())
-            }
+            })
+            .then((mediaStream) => {
+                this.stream = mediaStream
+            });
+        },
+        openCameraUser() {
+            this.camera.getUserMediaStream({
+                video: {
+                    facingMode: "user"
+                }
+            })
+            .then((mediaStream) => {
+                this.stream = mediaStream
+            });
+            // TODO: Flip the video element
+            // because it is weird
+        },
+        getCap() {
+            this.$refs.player.srcObject.getVideoTracks()
+            .forEach((track) => {
+                this.$refs.dev.innerText = JSON.stringify(track.getCapabilities())
+            })
         }
+    },
+    created() {
+        const camera = new PlantalkCamera();
+        this.camera = camera;
     }
+}
 </script>
