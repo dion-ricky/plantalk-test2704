448a630c9e309f283fc83cb9e3c73b04bff38261
dion-ricky <dionrickysptr@gmail.com>
2021-05-01 14:15:43 +0000

    Added playground for testing

---
+++ src/views/Playground.vue
@@ -0,0 +1,23 @@
+<template>
+    <h3>Playground</h3>
+    <PlantalkTextInput label="Email" v-model:textInput="email"  />
+    <p>{{ email }}</p>
+</template>
+
+<script>
+import PlantalkTextInput from "../components/Plantalk/TextInput"
+
+export default {
+    name: "Playground",
+    components: {
+        PlantalkTextInput
+    },
+    data: () => ({
+        email: ''
+    })
+}
+</script>
+
+<style>
+
+</style>
