cfe1218b8ebb0b85d9531cddd76ffb85ac1d5abd
dion-ricky <dionrickysptr@gmail.com>
2021-05-02 06:43:21 +0000

    Update playground

--- src/views/Playground.vue
+++ src/views/Playground.vue
@@ -2,18 +2,23 @@
     <h3>Playground</h3>
     <PlantalkTextInput label="Email" v-model:textInput="email"  />
     <p>{{ email }}</p>
+    <PlantalkPasswordInput label="Password" v-model:passwordInput="password" />
+    <p>{{ password }}</p>
 </template>
 
 <script>
 import PlantalkTextInput from "../components/Plantalk/TextInput"
+import PlantalkPasswordInput from "../components/Plantalk/PasswordInput";
 
 export default {
     name: "Playground",
     components: {
-        PlantalkTextInput
+        PlantalkTextInput,
+        PlantalkPasswordInput
     },
     data: () => ({
-        email: ''
+        email: '',
+        password: ''
     })
 }
 </script>
