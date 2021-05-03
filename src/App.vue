3105b3e1c1c4f836fdd1e7a4036f0d08749e013b
dion-ricky <dionrickysptr@gmail.com>
2021-05-03 04:59:10 +0000

    🎨 Update for better app update

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,103 @@
 <template>
   <div class="container">
-    <router-view/>
+    <router-view />
   </div>
+  <ActionToast
+    message="New version is available"
+    action="Update"
+	v-if="isUpdateAvailable"
+    @toast:action-clicked="updateServiceWorker"
+  />
 </template>
 
 <script>
-import PlantalkFirebase from './firebase'
+import PlantalkFirebase from "./firebase";
+import ActionToast from "./components/Plantalk/ActionToast";
 
 export default {
-  name: 'App',
+  name: "App",
+  components: {
+    ActionToast,
+  },
+  data: () => ({
+	  isUpdateAvailable: false,
+	  registration: null
+  }),
   methods: {
     requestNotification() {
       const messaging = PlantalkFirebase.getMessaging();
       messaging.requestNotification();
-    }
+    },
+    invokeServiceWorkerUpdateFlow(registration) {
+		this.registration = registration;
+		this.isUpdateAvailable = true;
+	},
+	updateServiceWorker() {
+		const registration = this.registration;
+
+		if (registration.waiting) {
+			// let waiting Service Worker know it should became active
+			registration.waiting.postMessage('SKIP_WAITING')
+		}
+	}
   },
   created() {
     const messaging = PlantalkFirebase.getMessaging();
     messaging.setOnMessageHandler((payload) => {
       console.log(payload);
     });
-  }
-}
+
+    // check if the browser supports serviceWorker at all
+    if ('serviceWorker' in navigator) {
+		let _this = this;
+
+		navigator.serviceWorker.getRegistrations().then((regs) => {
+			registration = regs[0];
+
+			// ensure the case when the updatefound event was missed is also handled
+            // by re-invoking the prompt when there's a waiting Service Worker
+            if (registration.waiting) {
+                _this.invokeServiceWorkerUpdateFlow(registration)
+            }
+			
+			registration.addEventListener('updatefound', () => {
+				if (registration.installing) {
+					// wait until the new Service worker is actually installed (ready to take over)
+					registration.installing.addEventListener('statechange', () => {
+						if (registration.waiting) {
+							// if there's an existing controller (previous Service Worker), show the prompt
+							if (navigator.serviceWorker.controller) {
+								_this.invokeServiceWorkerUpdateFlow(registration)
+							} else {
+								// otherwise it's the first install, nothing to do
+								console.log('Service Worker initialized for the first time')
+							}
+						}
+					})
+				}
+			})
+
+			let refreshing = false;
+
+			// detect controller change and refresh the page
+			navigator.serviceWorker.addEventListener('controllerchange', () => {
+				if (!refreshing) {
+					window.location.reload()
+					refreshing = true
+				}
+			})
+		})
+    }
+	// end service worker update detection
+  },
+};
 </script>
 
+<style lang="scss" scoped>
+div.container {
+	width: 100vw;
+	height: 100vh;
+	// position: absolute;
+	// z-index: 1;
+}
+</style>
