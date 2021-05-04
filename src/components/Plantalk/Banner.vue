1a8e277691d226765cb78610ac92c1bbff455752
dion-ricky <dionrickysptr@gmail.com>
2021-05-04 02:25:43 +0000

    🎨 Swiper with banner  component

---
+++ src/components/Plantalk/Banner.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="wrapper">
+        <img :src="imgSrc">
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: "Banner",
+    props: {
+        imgSrc: String
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    .wrapper {
+        border-radius: 12px;
+        cursor: pointer;
+
+        background-color: white;
+        box-shadow: 0px 16px 50px 0px rgba(0, 0, 0, 0.05);
+        -webkit-box-shadow: 16px 0px 50px 0px rgba(0, 0, 0, 0.05);
+        -moz-box-shadow: 0px 16px 50px 0px rgba(0, 0, 0, 0.05);
+
+        -webkit-tap-highlight-color: transparent;
+        -webkit-touch-callout: none;
+        -webkit-user-select: none;
+        -khtml-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
+
+        img {
+            width: 100%;
+            max-width: 100vw;
+            display: block;
+        }
+    }
+</style>
--- src/views/Playground.vue
+++ src/views/Playground.vue
@@ -1,41 +1,74 @@
 <template>
-    <div class="wrapper">
+    <div class="wrapper-pg">
 
-        <Reticle width="250px" height="250px" :state="state" />
+        <swiper
+            :slides-per-view="1"
+            :space-between="50"
+        >
+            <swiper-slide>
+                <Banner
+                    imgSrc="https://firebasestorage.googleapis.com/v0/b/plantalk-test2704.appspot.com/o/promotional%2Fbanner%2Fbnr_anti-stress-tips.png?alt=media&token=0eb7bad3-89ba-414b-8370-c8ac6ed07508"
+                />
+            </swiper-slide>
+            <swiper-slide>
+                <Banner
+                    imgSrc="https://firebasestorage.googleapis.com/v0/b/plantalk-test2704.appspot.com/o/promotional%2Fbanner%2Fbnr_anti-stress-tips.png?alt=media&token=0eb7bad3-89ba-414b-8370-c8ac6ed07508"
+                />
+            </swiper-slide>
+        </swiper>
+    </div>
+    
+  <!-- <Reticle width="250px" height="250px" :state="state" />
         <div>
             <button @click="state = 'sensing'">Sensing</button>
             <button @click="state = 'error'">Error</button>
             <button @click="state = 'loading'">Loading</button>
             <button @click="state = 'move-closer'">Move Closer</button>
-        </div>
-    </div>
+        </div> -->
 </template>
 
 <script>
-import Reticle from "../assets/plantalk/scanner/reticle"
+import Reticle from "../assets/plantalk/scanner/reticle";
+import Banner from "../components/Plantalk/Banner";
+
+import { Swiper, SwiperSlide } from "swiper/vue";
+import 'swiper/swiper.scss';
 
 export default {
-    name: "Playground",
-    components: {
-        Reticle
+  name: "Playground",
+  components: {
+    Reticle,
+    Banner,
+    Swiper,
+    SwiperSlide,
+  },
+  data: () => ({
+    state: "sensing",
+  }),
+  methods: {
+    onSwiper(swiper) {
+      console.log(swiper);
     },
-    data: () => ({
-        state: 'sensing'
-    }),
-    methods: {
-        
-    }
-}
+    onSlideChange() {
+      console.log("slide change");
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-    .wrapper {
-        width: 100vw;
-        height: 100vh;
-        background-color: black;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        flex-direction: column;
-    }
+.wrapper-pg {
+    position: relative;
+    height: 100%;
+//   width: 100vw;
+//   height: 100vh;
+//   padding: 2rem;
+//   background-color: #eeeeee;
+//   display: flex;
+//   align-items: center;
+//   justify-content: center;
+  //     flex-direction: column;
+}
+
+
 </style>
