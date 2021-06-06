8623389102c556734946873d0a88a639ace70652
dion-ricky <dionrickysptr@gmail.com>
2021-06-06 07:02:35 +0000

    🎨 Disable user-select on all element except input

--- src/components/IconButton.vue
+++ src/components/IconButton.vue
@@ -21,14 +21,6 @@ export default {
         display: inline-block;
 
         cursor: pointer;
-
-        -webkit-tap-highlight-color: transparent;
-        -webkit-touch-callout: none;
-        -webkit-user-select: none;
-        -khtml-user-select: none;
-        -moz-user-select: none;
-        -ms-user-select: none;
-        user-select: none;
     }
 
     .wrapper > * {
