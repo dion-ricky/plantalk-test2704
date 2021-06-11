559e98da50d5195980761d03be34305a78793798
dion-ricky <dionrickysptr@gmail.com>
2021-06-11 14:06:58 +0000

    Vision labeling relay service

---
+++ services/vision/index.js
@@ -0,0 +1,54 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const port = 8083
+
+app.use(cors())
+
+app.use(express.json({limit: '2mb'}))
+
+app.post('/', (req, res) => {
+    // console.log('oh hey');
+    // console.log(Buffer.from(req.body.image, "base64"))
+    // res.sendStatus(200);
+    const vision = new VisionLabeling(req, res);
+    vision.predict();
+})
+
+// app.listen(port,() => {
+//     console.log('Vision prediction started', port)
+// })
+
+exports.visionLabeling = app
+
+class VisionLabeling {
+    
+    constructor(req, res) {
+        this.req = req;
+
+        this.predictCallback = (prediction) => {
+            console.log(prediction)
+            res.status(200).send(prediction.labelAnnotations)
+        }
+    }
+
+    predict() {
+        const base64Image = this.req.body.image
+        const vision = require('@google-cloud/vision');
+        
+        const client = new vision.ImageAnnotatorClient();
+        
+        const buffer = Buffer.from(base64Image, "base64");
+        
+        client.labelDetection({
+            image: {
+                content: buffer
+            }
+        }).then((response) => {
+            this.predictCallback(response);
+        })
+        .catch((err) => {
+            console.error(err);
+        })
+    }
+}
